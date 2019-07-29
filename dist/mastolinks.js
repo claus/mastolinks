a19b7475e21440fb6d8526e45eeb9fcae8589d61
Claus Wahlers <claus@codeazur.com.br>
2019-07-29 02:28:32 -0300

    Cache feeds and statuses

--- dist/mastolinks.js
+++ dist/mastolinks.js
@@ -201,6 +201,8 @@ function extractLinks(status, instance) {
     );
 }
 
+const statusStore = {};
+
 function monitorFeed(instance) {
     const es = new EventSource(`https://${instance}/api/v1/streaming/public`);
 
@@ -218,34 +220,37 @@ function monitorFeed(instance) {
     }
 
     async function handleUpdate(event) {
-        const data = JSON.parse(event.data);
-        const links = await extractLinks(data);
-        if (links.length) {
-            const urls = links.map(link => {
-                let str = `- \x1B[33m${link.hrefClean}\x1B[39m`;
-                if (link.status >= 400) {
-                    str += ` \x1B[41m[${link.status}]\x1B[49m`;
-                }
-                if (link.hrefCanonical !== link.hrefClean) {
-                    str += `\n  \x1B[90m${link.hrefCanonical}\x1B[39m`;
-                }
-                if (link.href !== link.hrefCanonical) {
-                    str += `\n  \x1B[90m${link.href}\x1B[39m`;
-                }
-                return str;
-            });
-            const langCode = data.language || '??';
-            const lang = `\x1B[97m${langCode}\x1B[39m`;
-            const acct = `\x1B[97m${data.account.acct}\x1B[39m`;
-            const log = `[${lang}] [${acct}] [${instance}]`;
-            const emptyWidth =
-                72 -
-                data.account.acct.length -
-                langCode.length -
-                instance.length;
-            const empty = Array.from({ length: emptyWidth }).join(' ');
-            console.log(`\x1B[48;5;235m${log}${empty}\x1B[49m`);
-            console.log(urls.join('\n'));
+        const status = JSON.parse(event.data);
+        if (typeof statusStore[status.uri] === 'undefined') {
+            statusStore[status.uri] = status;
+            const links = await extractLinks(status);
+            if (links.length) {
+                const urls = links.map(link => {
+                    let str = `- \x1B[33m${link.hrefClean}\x1B[39m`;
+                    if (link.status >= 400) {
+                        str += ` \x1B[41m[${link.status}]\x1B[49m`;
+                    }
+                    if (link.hrefCanonical !== link.hrefClean) {
+                        str += `\n  \x1B[90m${link.hrefCanonical}\x1B[39m`;
+                    }
+                    if (link.href !== link.hrefCanonical) {
+                        str += `\n  \x1B[90m${link.href}\x1B[39m`;
+                    }
+                    return str;
+                });
+                const langCode = status.language || '??';
+                const lang = `\x1B[97m${langCode}\x1B[39m`;
+                const acct = `\x1B[97m${status.account.acct}\x1B[39m`;
+                const log = `[${lang}] [${acct}] [${instance}]`;
+                const emptyWidth =
+                    72 -
+                    status.account.acct.length -
+                    langCode.length -
+                    instance.length;
+                const empty = Array.from({ length: emptyWidth }).join(' ');
+                console.log(`\x1B[48;5;235m${log}${empty}\x1B[49m`);
+                console.log(urls.join('\n'));
+            }
         }
     }
 
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import EventSource from 'eventsource';
 import { extractLinks } from './utils/extractLinks';
 
+const feedStore = {};
+const statusStore = {};
+
 function monitorFeed(instance) {
     const es = new EventSource(`https://${instance}/api/v1/streaming/public`);
 
@@ -8,6 +11,8 @@ function monitorFeed(instance) {
     es.addEventListener('error', handleError);
     es.addEventListener('update', handleUpdate);
     es.addEventListener('delete', handleDelete);
+
+    feedStore[instance] = es;
 
     function handleOpen(event) {
         console.log(`\x1B[41m OPEN: [${instance}] \x1B[49m`, event);
@@ -18,34 +23,37 @@ function monitorFeed(instance) {
     }
 
     async function handleUpdate(event) {
-        const data = JSON.parse(event.data);
-        const links = await extractLinks(data, instance);
-        if (links.length) {
-            const urls = links.map(link => {
-                let str = `- \x1B[33m${link.hrefClean}\x1B[39m`;
-                if (link.status >= 400) {
-                    str += ` \x1B[41m[${link.status}]\x1B[49m`;
-                }
-                if (link.hrefCanonical !== link.hrefClean) {
-                    str += `\n  \x1B[90m${link.hrefCanonical}\x1B[39m`;
-                }
-                if (link.href !== link.hrefCanonical) {
-                    str += `\n  \x1B[90m${link.href}\x1B[39m`;
-                }
-                return str;
-            });
-            const langCode = data.language || '??';
-            const lang = `\x1B[97m${langCode}\x1B[39m`;
-            const acct = `\x1B[97m${data.account.acct}\x1B[39m`;
-            const log = `[${lang}] [${acct}] [${instance}]`;
-            const emptyWidth =
-                72 -
-                data.account.acct.length -
-                langCode.length -
-                instance.length;
-            const empty = Array.from({ length: emptyWidth }).join(' ');
-            console.log(`\x1B[48;5;235m${log}${empty}\x1B[49m`);
-            console.log(urls.join('\n'));
+        const status = JSON.parse(event.data);
+        if (typeof statusStore[status.uri] === 'undefined') {
+            statusStore[status.uri] = status;
+            const links = await extractLinks(status, instance);
+            if (links.length) {
+                const urls = links.map(link => {
+                    let str = `- \x1B[33m${link.hrefClean}\x1B[39m`;
+                    if (link.status >= 400) {
+                        str += ` \x1B[41m[${link.status}]\x1B[49m`;
+                    }
+                    if (link.hrefCanonical !== link.hrefClean) {
+                        str += `\n  \x1B[90m${link.hrefCanonical}\x1B[39m`;
+                    }
+                    if (link.href !== link.hrefCanonical) {
+                        str += `\n  \x1B[90m${link.href}\x1B[39m`;
+                    }
+                    return str;
+                });
+                const langCode = status.language || '??';
+                const lang = `\x1B[97m${langCode}\x1B[39m`;
+                const acct = `\x1B[97m${status.account.acct}\x1B[39m`;
+                const log = `[${lang}] [${acct}] [${instance}]`;
+                const emptyWidth =
+                    72 -
+                    status.account.acct.length -
+                    langCode.length -
+                    instance.length;
+                const empty = Array.from({ length: emptyWidth }).join(' ');
+                console.log(`\x1B[48;5;235m${log}${empty}\x1B[49m`);
+                console.log(urls.join('\n'));
+            }
         }
     }
 
